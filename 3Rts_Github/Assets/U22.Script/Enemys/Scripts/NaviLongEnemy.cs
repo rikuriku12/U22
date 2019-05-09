b449e01cbdd04b70c7f2ffc7c0a9268627d16bf8
rikuriku <kuroe1072@gmail.com>
2019-05-09 14:59:06 +0900

    [リクト]NavLongEnemyスクリプトの編集

--- 3Rts_Github/Assets/U22.Script/Enemys/Scripts/NaviLongEnemy.cs
+++ 3Rts_Github/Assets/U22.Script/Enemys/Scripts/NaviLongEnemy.cs
@@ -6,60 +6,84 @@ using UnityEngine.AI;
 public class NaviLongEnemy : MonoBehaviour
 {
     NavMeshAgent agent;
-    
+
+    [SerializeField] Animator e_Animator;
+
     Vector3 position;
 
-    private GameObject towe; //towe
-    private GameObject player; //player
-    private Transform targget; //目的地
-    GameObject nearObj;//近くのオブジェクト
+    private GameObject tower;// タワー
+    private GameObject player;// プレイヤー
+    private Transform targget;// 目的地
+    private GameObject nearObj;// 近くの特定のタグ付きオブジェクト
 
-    bool t_Flag;//タレットをターゲット
+    private bool npc_Flag;// プレイヤーNPCをターゲットにするフラグ
 
-    float agentDistance;//プレイヤー、敵間の距離
-    [SerializeField] float plyerDistance;
-    [SerializeField] float time;
-    private float searchTime = 0;
+    float agentDistance;// プレイヤー、敵間の距離
+    [SerializeField] float plyerDistance = 10f;// プレイヤーを検知する距離
+    [SerializeField] float stopDistance = 5f;// 停止距離
 
-
-
+    private float searchTime = 0;//serchTagの探す時間
 
     void Start()
     {
-        t_Flag = false;
+        npc_Flag = false;
 
-        
-        agent = gameObject.GetComponent<NavMeshAgent>();
-        towe = GameObject.FindWithTag("Towe");
-        player = GameObject.FindWithTag("Player");
-        if (towe)
+        agent = gameObject.GetComponent<NavMeshAgent>();//NaviMeshAgentのコンポーネントを取得
+        tower = GameObject.FindWithTag("Towe");// タワーを取得
+        player = GameObject.FindWithTag("Player");// プレイヤーを取得
+        //タワーがあれば
+        if (tower)
         {
-            targget = towe.transform;
+            //目的地をタワーに
+            targget = tower.transform;
         }
     }
 
     void Update()
     {
-        if (player)
+        //停止距離より離れていたら
+        if (!(agentDistance <= stopDistance))
         {
-            //EnemyとPlayerの距離
+            // 走るアニメーションの再生
+            e_Animator.SetBool("IsRun", true);
+        }
+        if (player)// playerの中にオブジェクトが入っていたら
+        {
+            // EnemyとPlayerの距離
             agentDistance = Vector3.Distance(this.agent.transform.position,
                                                 player.transform.position);
-
-            if (agentDistance <= plyerDistance && t_Flag == false)
+            // プレイヤーとの距離が近くなり、npcフラグオフのとき
+            if (agentDistance <= plyerDistance && npc_Flag == false)
             {
-                //目的地をプレイヤに設定
+                // 目的地をプレイヤに設定
                 targget = player.transform;
+
+                // 停止距離になったら
+                if (agentDistance <= stopDistance)
+                {
+                    //攻撃関数
+                    ArrowAttack();
+                }
+                else
+                {
+                    // Naviを切る
+                    agent.GetComponent<NavMeshAgent>().isStopped = false;
+                }
+            }
+            else
+            {
+                //目的地をタワーに設定
+                targget = tower.transform;
             }
         }
-        else if (t_Flag == false)
+        else if (npc_Flag == false)
         {
             //目的地をタワーに設定
-            targget = towe.transform;
+            targget = tower.transform;
         }
 
         //t_Flagがたったら
-        if (t_Flag == true)
+        if (npc_Flag == true)
         {
             searchTime += Time.deltaTime;
 
@@ -70,26 +94,27 @@ public class NaviLongEnemy : MonoBehaviour
                 //経過時間を初期化
                 searchTime = 0;
             }
-
             // nearObjがあれば
             if (nearObj)
             {
-                //目的地をタワーに
                 targget = nearObj.transform;
             }
             else // nearObjがなかったら
             {
-                t_Flag = false;
-                targget = towe.transform;
+                npc_Flag = false;
+                //目的地をタワーに
+                targget = tower.transform;
             }
         }
-
+        //ターゲットが入っていたら
         if (targget)
         {
+            // エージェント
             agent.SetDestination(targget.position);
         }
     }
-
+    
+    // タグ付きのオブジェクトを探す    
     GameObject serchTag(GameObject nowObj, string tagName)
     {
         float tmpDis = 0;           //距離用一時変数
@@ -112,23 +137,23 @@ public class NaviLongEnemy : MonoBehaviour
                 targetObj = obs;
             }
         }
-
-
         //最も近かったオブジェクトを返す
         //return GameObject.Find(nearObjName);
         return targetObj;
     }
 
-
-    private void OnTriggerEnter(Collider collider)
+    void ArrowAttack()
     {
-        //タレットの弾に当たったら
-        if (collider.gameObject.tag == "T_Bullet")
-        {
-            t_Flag = true;
-        }
+        // 走るアニメーションの停止
+        e_Animator.SetBool("IsRun", false);
+        // Naviを切る
+        agent.GetComponent<NavMeshAgent>().isStopped = true;
+        //プレイヤーの方向を向く
+        var aim = this.player.transform.position - this.transform.position;
+        var look = Quaternion.LookRotation(aim);
+        this.transform.localRotation = look;
+        //攻撃アニメーションの再生
+        e_Animator.SetTrigger("IsAttack");
     }
-
-    
 }
 
