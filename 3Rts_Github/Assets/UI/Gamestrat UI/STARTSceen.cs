643ddfc87b5fcf3c2ec2317004efb4d78751270d
Rize0214 <mesitukai1213@gmail.com>
2019-05-22 16:24:47 +0900

    スタートメニューシーン

---
+++ 3Rts_Github/Assets/UI/Gamestrat UI/STARTSceen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class STARTSceen : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Start"))
+        {
+            Invoke("FadeScene", 0f);
+        }
+    }
+
+    public void FadeScene()
+    {
+        FadeManager.Instance.LoadScene("SampleScene", 3f);
+    }
+}
---
+++ 3Rts_Github/Assets/UI/UI Assets/HP.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HP : MonoBehaviour
+{
+    public Image enemyHP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+    }
+}
