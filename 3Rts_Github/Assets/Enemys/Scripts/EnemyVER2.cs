7468fa6bf1895751ba4feaec19cbcd20ecb72dc7
Rize0214 <mesitukai1213@gmail.com>
2019-05-23 15:52:02 +0900

    敵アニメーションスクリプト「のけぞり」

---
+++ 3Rts_Github/Assets/Enemys/Scripts/EnemyVER2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int Hp;
+    Animation KnoCk;
+    // Start is called before the first frame update
+    void Start()
+    {
+        KnoCk = this.gameObject.GetComponent<Animation>();
+    }
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "NPC_Sword")
+        {
+            Hp -= 20;
+        }
+    }
+
+    public void OnTrggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "P_Sword")
+        {
+            KnoCk.Play();
+        }
+    }
+}
+
