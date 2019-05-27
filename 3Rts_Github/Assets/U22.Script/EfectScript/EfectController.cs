e05f8f4cca509107a4e846d170767e2ebb21f75c
syuto10101919 <nitta3733@gmail.com>
2019-05-27 16:20:01 +0900

    [北澤柊人]エフェクトコントローラー追加

---
+++ 3Rts_Github/Assets/U22.Script/EfectScript/EfectController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EfectController : MonoBehaviour
+{
+    public ParticleSystem particle;
+    //パーティクルエフェクト再生
+    void Effect()
+    {
+        particle.Play();
+    }
+}
