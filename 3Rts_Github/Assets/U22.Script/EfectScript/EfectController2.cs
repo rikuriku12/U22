aa8e2d174e8aec6cce7615e6027e802d0fe68080
syuto10101919 <nitta3733@gmail.com>
2019-05-30 13:56:12 +0900

    [北澤柊人]追加エフェクトスクリプト追加。

---
+++ 3Rts_Github/Assets/U22.Script/EfectScript/EfectController2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EfectController2 : MonoBehaviour
+{
+    public ParticleSystem particle;
+    //パーティクルエフェクト再生
+
+    void Start()
+    {
+        particle.Stop();  //最初は止める
+    }
+
+    void Effect2()
+    {
+        particle.Play();
+    }
+}
---
+++ 3Rts_Github/Assets/U22.Script/EfectScript/EfectController3.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EfectController3 : MonoBehaviour
+{
+    public ParticleSystem particle;
+    //パーティクルエフェクト再生
+
+    void Start()
+    {
+        particle.Stop();  //最初は止める
+    }
+
+    void Effect3()
+    {
+        particle.Play();
+    }
+}
+
