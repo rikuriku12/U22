9b759746d68ab725e554868976e41d7854f629c4
Hasuuuuu <1notes7bt@gmail.com>
2019-06-12 23:53:13 +0900

    タワークリスタルスクリプト実装・ボス動作確認

---
+++ 3Rts_Github/Assets/ControllTowerHpCrystal.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllTowerHpCrystal : MonoBehaviour
+{
+    public int Border;
+    GameObject Tower_GettingObj;
+    TowerCtrl Tower_Controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Tower_GettingObj = transform.parent.parent.gameObject;
+        Tower_Controller = Tower_GettingObj.GetComponent<TowerCtrl>();
+
+        if(Border >= Tower_Controller.Hp)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
