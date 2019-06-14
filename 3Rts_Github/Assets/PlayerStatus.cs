03d01ff7811156620bdf8b9de1e717579bbdd5ee
syuto10101919 <nitta3733@gmail.com>
2019-06-14 10:40:25 +0900

    [北澤柊人]ダメージエフェクト完成

--- 3Rts_Github/Assets/PlayerStatus.cs
+++ 3Rts_Github/Assets/PlayerStatus.cs
@@ -6,11 +6,16 @@ public class PlayerStatus : MonoBehaviour
 {
     // Start is called before the first frame update
     public float PHp;
-    public ParticleSystem damegepar;
+    public ParticleSystem DamegeArrow;
+    public ParticleSystem DamegeSword;
+    public ParticleSystem DamegeTower;
+
     // Start is called before the first frame update
     void Start()
     {
-        damegepar.Stop();//最初は停止
+        DamegeArrow.Stop();//最初は停止
+        DamegeSword.Stop();
+        DamegeTower.Stop();
     }
 
     private void Update()
@@ -23,33 +28,33 @@ public class PlayerStatus : MonoBehaviour
 
     }
     // Update is called once per frame
-    public void OnCollisionEnter(Collision collision)//矢が刺さるように
-    {
+    public void OnCollisionEnter(Collision collision)
+    { 
         if (collision.gameObject.tag == "EnemyArrow")
         {
             PHp -= 50;
 
+            Debug.Log("5");
             //ダメージ後にパーティクル発生
-            damegepar.Play();
+            DamegeArrow.Play();
         }
-    }
 
-    public void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == "Enemy_Sword")
+        if (collision.gameObject.tag == "Enemy_Sword")
         {
             PHp -= 20;
 
+            Debug.Log("5");
             //ダメージ後にパーティクル発生
-            damegepar.Play();
+            DamegeSword.Play();
         }
 
-        if (other.gameObject.tag == "TowerBurret")
+        if (collision.gameObject.tag == "TowerBurret")
         {
             PHp -= 100;
 
+            Debug.Log("5");
             //ダメージ後にパーティクル発生
-            damegepar.Play();
+            DamegeTower.Play();
         }
     }
 }
