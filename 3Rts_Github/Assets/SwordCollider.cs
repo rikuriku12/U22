13c08b37a57c058502923f8561f21af97918bb8c
Hasuuuuu <1notes7bt@gmail.com>
2019-06-18 15:36:51 +0900

    いろいろ調整

---
+++ 3Rts_Github/Assets/SwordCollider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+public class SwordCollider : MonoBehaviour
+{
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButton("joystick X") && )
+        {
+            GetComponent<BoxCollider>().enabled = true;
+        }
+
+        else
+        {
+            GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+}
