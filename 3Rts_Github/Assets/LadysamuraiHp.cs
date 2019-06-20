0acf1b5055d3e559e78201370cd823d53cee7533
Hasuuuuu <1notes7bt@gmail.com>
2019-06-20 11:25:56 +0900

    いろいろ調整

---
+++ 3Rts_Github/Assets/LadysamuraiHp.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadysamuraiHp : MonoBehaviour
+{
+    [SerializeField] int Hp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Hp <= 0)
+        {
+            Destroy(gameObject.transform.root.gameObject);
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Enemy_Sword")
+        {
+            Hp -= 10; 
+        }
+
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy_Arrow")
+        {
+            Hp -= 10;
+        }
+    }
+}
