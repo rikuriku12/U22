936c8466fbbf53fdedff6e5b490efb4eb04b95f6
rikuriku <kuroe1072@gmail.com>
2019-06-25 15:01:29 +0900

    敵HPバー表示に関する編集

--- 3Rts_Github/Assets/Enemys/Scripts/Hpvar.cs
+++ 3Rts_Github/Assets/Enemys/Scripts/Hpvar.cs
@@ -5,22 +5,27 @@ using UnityEngine;
 public class Hpvar : MonoBehaviour
 {
     GameObject player;
+    [SerializeField]GameObject enemy;
     [SerializeField]bool pcr;                       //HPバーの可視化
-    // Start is called before the first frame update
+    [SerializeField]bool isHp;
+
     void Start()
     {
         transform.GetChild(0).gameObject.SetActive(false);
         player = GameObject.FindWithTag("Player");
-        
+        //enemy = transform.parent.gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
         pcr = player.GetComponent<PlayerController>().HPvar;
-        if (pcr == true)
+        isHp = enemy.GetComponent<MeleeEnemyHp>().hpUi;
+
+        if (pcr == true || isHp == true)
         {
             transform.GetChild(0).gameObject.SetActive(true);
+
         }
         else
         {
