dee6917464cddd38743ae7c374f077bb65ae951b
Hasuuuuu <1notes7bt@gmail.com>
2019-06-25 15:48:42 +0900

    殴った時にhp表示実装

---
+++ 3Rts_Github/Assets/Enemys/Scripts/Hpvar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hpvar : MonoBehaviour
+{
+    GameObject player;
+    [SerializeField]bool pcr;                       //HPバーの可視化
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.GetChild(0).gameObject.SetActive(false);
+        player = GameObject.FindWithTag("Player");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pcr = player.GetComponent<PlayerController>().HPvar;
+        if (pcr == true)
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+        }
+        else
+        {
+            transform.GetChild(0).gameObject.SetActive(false);
+        }
+    }
+}
