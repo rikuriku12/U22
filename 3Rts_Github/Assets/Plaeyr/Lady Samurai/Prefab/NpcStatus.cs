145b34798861066a1aff297a692d77784345641d
Hasuuuuu <1notes7bt@gmail.com>
2019-06-26 11:56:28 +0900

    ステージ調整・NPCのhp実装

---
+++ 3Rts_Github/Assets/Plaeyr/Lady Samurai/Prefab/NpcStatus.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcStatus : MonoBehaviour
+{
+    public int Hp; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Hp <= 0)
+        {
+            Destroy(transform.root.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag  == "Enemy_Sword")
+        {
+            Hp -= 30;
+        }
+
+        if (other.gameObject.tag == "Enemy_Arrow")
+        {
+            Hp -= 10;
+        }
+
+        if(other.gameObject.tag  == "TowerBurret")
+        {
+            Hp -= 50;
+        }
+    }
+}
